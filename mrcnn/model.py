4df39d4c27183ff347de7b07d5e0520fc5eccecc
Claris Gu <55867918+jgu13@users.noreply.github.com>
2021-08-28 09:11:26 -0400

    update build_rpn_target
    Changed box refinement to use utils.box_refinement. Changed positive box threshold from 0.7 to 0.6.

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -1482,7 +1482,7 @@ def build_rpn_targets(image_shape, anchors, gt_class_ids, gt_boxes, config):
     overlaps = utils.compute_overlaps(anchors, gt_boxes)
 
     # Match anchors to GT Boxes
-    # If an anchor overlaps a GT box with IoU >= 0.7 then it's positive.
+    # If an anchor overlaps a GT box with IoU >= 0.6 then it's positive.
     # If an anchor overlaps a GT box with IoU < 0.3 then it's negative.
     # Neutral anchors are those that don't match the conditions above,
     # and they don't influence the loss function.
@@ -1499,7 +1499,7 @@ def build_rpn_targets(image_shape, anchors, gt_class_ids, gt_boxes, config):
     gt_iou_argmax = np.argwhere(overlaps == np.max(overlaps, axis=0))[:,0]
     rpn_match[gt_iou_argmax] = 1
     # 3. Set anchors with high overlap as positive.
-    rpn_match[anchor_iou_max >= 0.7] = 1
+    rpn_match[anchor_iou_max >= 0.6] = 1
 
     # Subsample to balance positive and negative anchors
     # Don't let positives be more than half the anchors
@@ -1514,7 +1514,7 @@ def build_rpn_targets(image_shape, anchors, gt_class_ids, gt_boxes, config):
     extra = len(ids) - (config.RPN_TRAIN_ANCHORS_PER_IMAGE -
                         np.sum(rpn_match == 1))
     if extra > 0:
-        # Rest the extra ones to neutral
+        # Reset the extra ones to neutral
         ids = np.random.choice(ids, extra, replace=False)
         rpn_match[ids] = 0
 
@@ -1526,26 +1526,26 @@ def build_rpn_targets(image_shape, anchors, gt_class_ids, gt_boxes, config):
     for i, a in zip(ids, anchors[ids]):
         # Closest gt box (it might have IoU < 0.7)
         gt = gt_boxes[anchor_iou_argmax[i]]
+	rpn_bbox[ix] = utils.box_refinement(a,gt)
+#         # Convert coordinates to center plus width/height.
+#         # GT Box
+#         gt_h = gt[2] - gt[0]
+#         gt_w = gt[3] - gt[1]
+#         gt_center_y = gt[0] + 0.5 * gt_h
+#         gt_center_x = gt[1] + 0.5 * gt_w
+#         # Anchor
+#         a_h = a[2] - a[0]
+#         a_w = a[3] - a[1]
+#         a_center_y = a[0] + 0.5 * a_h
+#         a_center_x = a[1] + 0.5 * a_w
 
-        # Convert coordinates to center plus width/height.
-        # GT Box
-        gt_h = gt[2] - gt[0]
-        gt_w = gt[3] - gt[1]
-        gt_center_y = gt[0] + 0.5 * gt_h
-        gt_center_x = gt[1] + 0.5 * gt_w
-        # Anchor
-        a_h = a[2] - a[0]
-        a_w = a[3] - a[1]
-        a_center_y = a[0] + 0.5 * a_h
-        a_center_x = a[1] + 0.5 * a_w
-
-        # Compute the bbox refinement that the RPN should predict.
-        rpn_bbox[ix] = [
-            (gt_center_y - a_center_y) / a_h,
-            (gt_center_x - a_center_x) / a_w,
-            np.log(gt_h / a_h),
-            np.log(gt_w / a_w),
-        ]
+#         # Compute the bbox refinement that the RPN should predict.
+#         rpn_bbox[ix] = [
+#             (gt_center_y - a_center_y) / a_h,
+#             (gt_center_x - a_center_x) / a_w,
+#             np.log(gt_h / a_h),
+#             np.log(gt_w / a_w),
+#         ]
         # Normalize
         rpn_bbox[ix] /= config.RPN_BBOX_STD_DEV
         ix += 1
